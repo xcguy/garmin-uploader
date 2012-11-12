82b2f905607911a135d6d314522b15e5402dbd5d
David E. Lotton <yellow56@gmail.com>
2012-11-11 19:42:04 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+try:
+	from ez_setup import use_setuptools
+	use_setuptools()
+except ImportError:
+	pass
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name = "GcpUploader",
+	version = "2012.11.08",
+	author = "David E. Lotton",
+	author_email = "yellow56@gmail.com",
+	description = "A tool to upload FIT, GPX, and TCX files to the Garmin Connect web site.",
+	url = "http://sourceforge.net/projects/gcpuploader/",
+	license = "GPL",
+	keywords = "GARMIN CONNECT GPS TCX GPX FIT UPLOAD UPLOADER",
+
+	package_dir = {'': 'lib'},
+	scripts = ["gupload.py"],
+
+	py_modules = ['ez_setup', 'UploadGarmin', 'MultipartPostHandler'],
+
+	install_requires = [
+		"logging",
+		"simplejson",
+	],
+
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"Environment :: Console",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Programming Language :: Python",
+		"Topic :: Utilities",
+	],
+)
