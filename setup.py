f0e8d89829dcb3062cdf1ed5f61b06c4f1bb0d42
David E. Lotton <yellow56@gmail.com>
2014-02-16 15:16:32 -0700

    Update version number in setup.py.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
 	name = "GcpUploader",
-	version = "2013.05.05",
+	version = "2014.02.16",
 	author = "David E. Lotton",
 	author_email = "yellow56@gmail.com",
 	description = "A tool to upload FIT, GPX, and TCX files to the Garmin Connect web site.",
